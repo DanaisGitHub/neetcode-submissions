6a91e9dddfe09caf7e42bcb4f786bb58031dc08a
Dan Zerai <dan.zerai22@gmail.com>
2026-05-15 13:15:22 +0100

    Add: is-palindrome - submission-6

---
+++ Data Structures & Algorithms/is-palindrome/submission-6.go
@@ -0,0 +1,30 @@
+func isPalindrome(s string) bool {
+	s = strings.ToLower(s)
+	l,r:=0,len(s)-1
+
+	for l<r{
+		if !isAlphaNumeric(byte(s[l])){
+			l++
+			continue
+		}
+		if !isAlphaNumeric(byte(s[r])){
+			r--
+			continue
+		}
+
+	if s[l] != s[r]{
+		return false
+	} 
+	l++
+	r--
+	}
+	return true
+
+
+
+}
+
+func isAlphaNumeric(c byte) bool{
+	return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9')
+}
+
